9a5529fd08deb32c705a2d614d0a97651a312bc2
Rithik1010 <rithikroshan018@gmail.com>
2022-08-23 01:50:24 +0530

    Add refresh token

--- authServer.js
+++ authServer.js
@@ -7,6 +7,20 @@ const jwt = require("jsonwebtoken");
 //Lets our application use json from the body of the request.
 app.use(express.json());
 
+//Storing refresh tokens locally
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+    const refreshToken = req.body.token;
+    if(refreshToken == null)                    return res.sendStatus(401);
+    if(!refreshTokens.includes(refreshToken))   return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if(err) res.sendStatus(403);
+        const accessToken = generateAccessToken({ name : user.name });
+        res.json({accessToken: accessToken});
+    });
+})
+
 app.post("/login", (req, res) => {
     //Authenticate the user.
 
@@ -15,11 +29,12 @@ app.post("/login", (req, res) => {
 
     const accessToken = generateAccessToken(user);
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
     res.json({ accessToken: accessToken, refreshToken: refreshToken });
 })
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20s" });
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
 }
 
 app.listen(4000, () => {
--- server.js
+++ server.js
@@ -22,16 +22,6 @@ app.get("/posts", authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name));
 });
 
-app.post("/login", (req, res) => {
-    //Authenticate the user.
-
-    const username = req.body.username;
-    const user = { name: username }
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
-})
-
 // Create a middleware to authenticate user
 function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
